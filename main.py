fa072216e0e1fd5af76ffd9aadf2f7e00818672d
Laszlo Adam Kohajda <laszloadamkohajda@users.noreply.github.com>
2025-04-25 19:59:50 +0200

    Removed test example, created initializations

--- main.py
+++ main.py
@@ -4,24 +4,33 @@ from flight import DomesticFlight, InternationalFlight
 from booking import Booking
 
 
+def flights_init(airline):
+    airline.flight_append(DomesticFlight("A111", "Debrecen", 19490))
+    airline.flight_append(InternationalFlight("A222", "London", 46890))
+    airline.flight_append(DomesticFlight("A333", "Budapest", 23290))
+
+    return airline
+
+
+def bookings_init(airline):
+    booking_system = Booking()
+    booking_system.ticket_book("Gipsz Jakab", airline.flights[0])
+    booking_system.ticket_book("Kemp Elek", airline.flights[1])
+    booking_system.ticket_book("Kukor Ica", airline.flights[0])
+    booking_system.ticket_book("Akciós Áron", airline.flights[0])
+    booking_system.ticket_book("Lassú Anett", airline.flights[1])
+    booking_system.ticket_book("Minta Márton", airline.flights[2])
+
+    return booking_system
+
+
 def main() -> None:
 
-    airline1 = Airline("Wizz Air", "W")
-    dom_flight1 = DomesticFlight("W12345", "Debrecen", 21490)
-    int_flight1 = InternationalFlight("W54321", "London", 43890)
-
-    airline1.flight_append(dom_flight1)
-    airline1.flight_append(int_flight1)
-
-    booking_system = Booking()
-    print(booking_system.ticket_book("Gipsz Jakab", int_flight1))
-    print(booking_system.ticket_book("Kemp Elek", dom_flight1))
-    print(booking_system.bookings_list())
-    print(booking_system.ticket_cancel("Gipsz Jakab", "W54321"))
-    print(booking_system.bookings_list())
+    airline = flights_init(Airline("Betyár Társaság", "A"))
+    booking_system = bookings_init(airline)
 
     input()
-    menu_main()
+    menu_main(booking_system, airline)
 
 
 if __name__ == "__main__":
