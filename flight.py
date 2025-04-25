11d2741be23dcc0e7fe199faadf95e0b6a8cf149
Laszlo Adam Kohajda <laszloadamkohajda@users.noreply.github.com>
2025-04-25 17:51:01 +0200

    Created classes to handle flights

---
+++ flight.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class Flight(ABC):
+    def __init__(self, id, destination, price):
+        self.id = id
+        self.destination = destination
+        self.price = price
+
+    @abstractmethod
+    def flight_information():
+        pass
+
+
+class DomesticFlight(Flight):
+    def __init__(self, id, destination, price):
+        super().__init__(id, destination, price)
+
+    def flight_information(self):
+        return f"Belföldi járat - Azonosító: {self.id}, Úticél: {self.destination}, Jegyár: {self.price} Ft\n"
+
+
+class InternationalFlight(Flight):
+    def __init__(self, id, destination, price):
+        super().__init__(id, destination, price)
+
+    def flight_information(self):
+        return f"Nemzetközi járat - Azonosító: {self.id}, Úticél: {self.destination}, Jegyár: {self.price} Ft\n"
+
+
+def flights_init():
+    pass
