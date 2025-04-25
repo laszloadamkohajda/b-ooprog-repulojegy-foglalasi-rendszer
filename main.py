c80a3fa3b00650237dc3df279de5ad41c217e33f
Laszlo Adam Kohajda <laszloadamkohajda@users.noreply.github.com>
2025-04-25 18:11:45 +0200

    Updated main.py to test booking system

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from menu import menu_main
+from airline import Airline
+from flight import DomesticFlight, InternationalFlight
+from booking import Booking
 
 
 def main() -> None:
+
+    airline1 = Airline("Wizz Air", "W")
+    dom_flight1 = DomesticFlight("W12345", "Debrecen", 21490)
+    int_flight1 = InternationalFlight("W54321", "London", 43890)
+
+    airline1.flight_append(dom_flight1)
+    airline1.flight_append(int_flight1)
+
+    booking_system = Booking()
+    print(booking_system.ticket_book("Gipsz Jakab", int_flight1))
+    print(booking_system.ticket_book("Kemp Elek", dom_flight1))
+    print(booking_system.bookings_list())
+    print(booking_system.ticket_cancel("Gipsz Jakab", "W54321"))
+    print(booking_system.bookings_list())
+
+    input()
     menu_main()
 
 
