df6aa9528be819856e2a1e9ba37e8013640bff05
Laszlo Adam Kohajda <laszloadamkohajda@users.noreply.github.com>
2025-04-25 20:06:12 +0200

    Show fligths and Show bookings are working

--- menu.py
+++ menu.py
@@ -12,32 +12,40 @@ def menu_main_print() -> None:
             print(f"{menu_item[0]} - {menu_item[1]}")
 
 
-def menu_sub_print(menu_selected) -> None:
+def menu_sub_print(menu_selected, booking_system, airline) -> None:
     if menu_selected == 1:
         clear_screen()
-        print("Járatok")
+        print("Járatok\n")
+        for flight_item in airline.flight_information():
+            print(f"{flight_item}")
         input()
+
     elif menu_selected == 2:
         clear_screen()
-        print("Foglalások")
+        print("Foglalások\n")
+        for booking_item in booking_system.bookings_list():
+            print(f"{booking_item}")
         input()
+
     elif menu_selected == 3:
         clear_screen()
-        print("Lemondás")
+        print("Lemondás\n")
         input()
+
     elif menu_selected == 4:
         clear_screen()
-        print("Járataim")
+        print("Járataim\n")
         input()
+
     elif menu_selected == 9:
         clear_screen()
         print("A program bezárult.\n")
 
 
-def menu_main() -> None:
+def menu_main(booking_system, airline) -> None:
     menu_selected = 0
 
     while menu_selected != 9:
         menu_main_print()
         menu_selected = int(input("\nVálasszon egy menüpontot: "))
-        menu_sub_print(menu_selected)
+        menu_sub_print(menu_selected, booking_system, airline)
